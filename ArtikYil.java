bbe2ffba9c5b496539943d7a8d8498d634192348
Fatmaakarsu <20120205020@ismu.edu.tr>
2021-10-15 09:36:25 +0300

    ArtikYil added.

---
+++ ArtikYil.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Yıl Giriniz : ");
+        int year = input.nextInt();
+
+        if(year%4==0){
+            System.out.println(year+" bir artık yıldır!");
+        }
+        else if(year%100==0){
+            if(year%400==0){
+                System.out.println(year+" bir artık yıldır!");
+            }
+        }
+        else{
+            System.out.println(year+" bir artık yıl değildir!");
+        }
+    }
+}
